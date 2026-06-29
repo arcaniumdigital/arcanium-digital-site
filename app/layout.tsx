e0a3143e48068f0ee69c5f1b89f60b81c7da4dba
arcaniumdigital <297441882+arcaniumdigital@users.noreply.github.com>
2026-06-29 12:44:12 +1000

    Move Meta Pixel script to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { ReactNode } from "react"
 import type { Metadata } from 'next'
-import Script from 'next/script'
 import { Inter, JetBrains_Mono, Space_Grotesk } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import { siteDescription, siteName, siteUrl } from '@/lib/site'
@@ -55,6 +54,19 @@ export const metadata: Metadata = {
   },
 }
 
+const metaPixelScript = `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${metaPixelId}');
+fbq('track', 'PageView');
+`;
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -62,21 +74,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script id="meta-pixel" dangerouslySetInnerHTML={{ __html: metaPixelScript }} />
+      </head>
       <body className={`${inter.variable} ${spaceGrotesk.variable} ${jetbrainsMono.variable} font-sans antialiased`}>
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${metaPixelId}');
-            fbq('track', 'PageView');
-          `}
-        </Script>
         <noscript>
           <img
             height="1"
